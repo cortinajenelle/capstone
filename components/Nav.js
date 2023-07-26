f51d5bacb847f5e99adf91e8e368eb71c9c1b38a
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-07-26 19:00:36 -0400

    Capstone

--- components/Nav.js
+++ components/Nav.js
@@ -2,15 +2,7 @@ import html from "html-literal";
 
 export default links => html`
   <nav>
-    <i class="fas fa-bars">
-    <ul>
-      <li>Home</li>
-      <li>Origins</li>
-      <li>Farmers</li>
-      <li>Freelancers</li>
-      <li>Contact</li>
-    </ul>ç
-    </i>
+    <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
       ${links.map(link => {
         return `<li>
