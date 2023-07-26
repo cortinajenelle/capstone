0fbc3cf1cebaa83f531496e785b300110bee63a9
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-07-25 20:51:03 -0400

    Capstone

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+import html from "html-literal";
+
+export default () => html`
+  <footer>
+    &copy; 2023
+    <a href="https://github.com/cortinajenelle">Cortina Jenelle Caldwell</a>
+  </footer>
+`;
