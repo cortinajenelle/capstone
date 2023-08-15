7e54fb74d9c93fcf3c8a0fff1217790e3829ec42
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-08-15 01:52:53 -0400

    Capstone

--- components/Header.js
+++ components/Header.js
@@ -2,13 +2,5 @@ import html from "html-literal";
 
 export default state => html`
   <header>
-    <img
-      src="./assets/Caldwell_CortinaCapstoneLogoTransparent.png"
-      width="60px"
-      height="40px"
-      id="logo"
-      alt="Logo image"
-      style="margin-left: 10px;"
-    />
   </header>
 `;
--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,10 @@ import html from "html-literal";
 
 export default links => html`
   <nav>
+    <div class="logo" alt text="landScape"></div>
+    <a href="https://landscape-capstone.netlify.app">
+      <img src="./assets/logo.png" alt="Logo image" />
+    </a>
     <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
       ${links
