b2f1fbba5a7c0fbb85ecc9a4272f477ef259d702
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-08-18 17:14:37 -0400

    Capstone

--- store/Contact.js
+++ store/Contact.js
@@ -1,5 +1,17 @@
 export default {
   header: "Contact",
   view: "Contact",
-  map: {}
+  farms: [
+    {
+      name: "Bird Fork Farm",
+      address1: "261 Lewis Cross Road",
+      address2: "",
+      city: "Dunlap",
+      state: "TN",
+      zip: "37327",
+      commodity: ["veggies", "flowers", "eggs", "honey"],
+      opportunity: ["marketing", "website"]
+    }
+  ],
+  pins: []
 };
