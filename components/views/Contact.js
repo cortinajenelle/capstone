28b8c2e8c960c424339bd0808705ae6a63a8a132
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-08-21 04:50:47 -0400

    Capstone

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -1,6 +1,241 @@
 import html from "html-literal";
 
 export default state => html`
-  <h2>Regional Farmer Map</h2>
+  <h1>Contact</h1>
+  <br />
   <div id="map"></div>
+  <div>
+    <br />
+    <p>
+      Are you a local farmer in need of a digital freelancer to support with web
+      design, operations support, social media, file organizing, web
+      development, marketing+branding or copywriting?
+    </p>
+  </div>
+  <section id="form">
+    <form id="form" method="POST" action="">
+      <h2>Offer Yer Farmgoods & Get Digital Help:</h2>
+      <div>
+        <label for="name">Farm Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Enter Farm Name"
+          required
+        />
+      </div>
+      <div>
+        <label for="address1">Address1:</label>
+        <input
+          type="text"
+          name="address1"
+          id="address1"
+          placeholder="Enter Address"
+          required
+        />
+      </div>
+      <div>
+        <label for="address2">Address2:</label>
+        <input
+          type="text"
+          name="address2"
+          id="address2"
+          placeholder="Suite, Unit or Box #"
+          required
+        />
+      </div>
+      <div>
+        <label for="city">City:</label>
+        <input
+          type="text"
+          name="city"
+          id="city"
+          placeholder="Enter City"
+          required
+        />
+      </div>
+      <div>
+        <label for="state">State:</label>
+        <select id="state" name="state">
+          <option value="">Select a State</option>
+          <option value="TN">Tennessee</option>
+          <option value="GA">Georgia</option>
+          <option value="AL">Alabama</option>
+          <option value="MS">Mississippi</option>
+          <option value="KY">Kentucky</option>
+        </select>
+      </div>
+      <div>
+        <label for="zip">Zip Code:</label>
+        <input
+          type="text"
+          name="zip"
+          id="zip"
+          placeholder="Enter Zip Code"
+          required
+        />
+      </div>
+      <div>
+      <br/>
+        <label for="farmgoods">Farmgoods:</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox1"
+          class="items1"
+          name="farmgoods"
+          value="Farmgoods"
+        />
+        <label for="top1">eggs</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox2"
+          class="items1"
+          name="farmgoods"
+          value="eggs"
+        />
+        <label for="top2">honey</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox3"
+          class="items1"
+          name="farmgoods"
+          value="honey"
+        />
+        <label for="top3">wildflowers</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox4"
+          class="items1"
+          name="farmgoods"
+          value="wildflowers"
+        />
+        <label for="top4">CSA produce box</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox5"
+          class="items1"
+          name="farmgoods"
+          value="vegetables"
+        />
+        <label for="top5">dairy products</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox6"
+          class="items1"
+          name="farmgoods"
+          value="dairy products"
+          />
+        <label for="top6">organic, humane meat</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox7"
+          class="items1"
+          name="farmgoods"
+          value="meat"
+          />
+        <label for="top7">herbal teas</label>
+      <div>
+      <br/>
+        <label for="freelancers">Opportunities:</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox1"
+          class="items1"
+          name="freelancers"
+          value="web design and development"
+        />
+        <label for="top1">web design & dev</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox2"
+          class="items1"
+          name="freelancers"
+          value="graphic design"
+        />
+        <label for="top2">graphic design</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox3"
+          class="items1"
+          name="freelancers"
+          value="branding"
+        />
+        <label for="top3">branding</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox4"
+          class="items1"
+          name="freelancers"
+          value="membership"
+        />
+        <label for="top4">membership</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox5"
+          class="items1"
+          name="freelancers"
+          value="social media"
+        />
+        <label for="top5">social media</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox6"
+          class="items1"
+          name="freelancers"
+          value="cybersecurity"
+          />
+        <label for="top6">cybersecurity</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox7"
+          class="items1"
+          name="freelancers"
+          value="marketing"
+          />
+        <label for="top7">marketing</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox7"
+          class="items1"
+          name="freelancers"
+          value="data analysis"
+          />
+        <label for="top7">data analysis</label>
+        <input
+          type="checkbox"
+          id="id_of_checkbox7"
+          class="items1"
+          name="freelancers"
+          value="copywriting"
+          />
+        <label for="top7">copywriting</label>
+      </div>
+      <br/>
+      <input type="hidden" name="customer" id="customer" value="landScape" />
+      <input type="submit" name="submit" value="Submit Farm" />
+    </form>
+  </section>
+  <div><p>###</p></div>
+  <div>
+    <p>
+      Are you a digital freelancer wanting to support your local agricultural
+      ecosystem? Or maybe you just want to eat a bit healthier in between web
+      development sprints? We've got you covered!
+    </p>
+    <h2>Current Opportunities with Farmers Include:</h2>
+    <ul>
+      <li>Web Design</li>
+      <li>Graphic Design</li>
+      <li>Brand Identity & Strategy</li>
+      <li>Mailing Lists & Membership</li>
+      <li>Social Media Management</li>
+      <li>Web Development</li>
+      <li>Cybersecurity</li>
+      <li>Marketing & Promotions</li>
+      <li>Data & Business Analysis</li>
+      <li>Copywriting</li>
+    </ul>
+    <button class="button"><a href="https://landscape-capstone.netlify.app/contact"><h3>Apply to Freelance</h3></a>
+  </div>
 `;
--- components/views/Origins.js
+++ components/views/Origins.js
@@ -1,12 +1,57 @@
 import html from "html-literal";
 
-export default () =>html`<section id="origins">
-    <h2>Vivamus ac justo eu nisi</h2>
-    <img src="https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-    alt="me">
-    />
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sagittis nulla. Etiam semper mauris a quam tempus, non feugiat massa posuere. Morbi vitae nisi aliquam, semper diam id, molestie leo. In hac habitasse platea dictumst. Nulla et enim vel elit dictum viverra. Nunc pharetra lacinia arcu sit amet dignissim. Pellentesque ut mi nulla.</p>
-    <p>Quisque at hendrerit libero, eget interdum lectus. Etiam facilisis leo nulla, sit amet imperdiet nunc molestie vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur consectetur felis a purus volutpat, sed finibus magna iaculis. Pellentesque tristique tristique turpis nec vehicula. Maecenas varius quis tellus id mollis. Vivamus ut ultrices ligula.</p>
-    <p>Etiam egestas consectetur gravida. Nulla mollis suscipit sapien sed fermentum. Integer vitae eros a magna vulputate aliquam. Suspendisse sed pulvinar augue, auctor mollis lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eleifend diam quis libero sollicitudin efficitur. Nullam sapien eros, tempor eget vulputate ut, interdum vel orci. Donec sit amet tempor mi. Nam feugiat cursus egestas. Suspendisse eget orci et ex mattis ornare tempor non tellus. Suspendisse gravida neque in urna congue bibendum. Duis dui odio, pharetra nec odio ac, ornare vulputate nibh.</p>
+export default () => html`
+  <div id="fas fa-bars"></div>
+  <h1>Origins</h1>
+  <br />
+  <section id="about">
+    <p>
+      According to recent surveys of young farmers by the American Farmlands
+      Trust, 35% of young farmers name the cost of production as greater than
+      the price received for the products.. In that same survey, it was reported
+      that 41% of young farmers name finding access to capital to grow their
+      business as very or extremely challenging. With this web application,
+      features become available to farmers who may have otherwise been invisible
+      to the greater community while also attracting quality talent through a
+      built-in bartering system. In this case, the freelancer is supporting a
+      local farm; improving health and overall quality of life by consuming
+      locally grown foods and produce; and community is fostered through a
+      shared mission to protect and preserve natural resources. This is a way
+      technology can help do that.
+    </p>
+    <p>
+      In my life, the worlds of nature and technology collide, often. For my
+      capstone project, I am building a web app that supports farmers meet their
+      creative, development and digital needs by hosting an exchange network
+      that connects them to digital freelancers with time to offer in exchange
+      for CSA, reduced venue rental or other products/services already offered
+      at the farm. the idea is to connect two end users - farmers and
+      freelancers - who are traditionally not connected, but in order to turn
+      the tide on environmental restoration perhaps should be. even with very
+      different skill sets, each end user is getting a need met and putting
+      action behind their concern for the environment or building healthy
+      communities.
+    </p>
+    <p>This web application aims to address several real-world, sustainability issues: </p>
+    <ul>
+      <li>connects farmers with high quality, digital talent they may not otherwise have access to for
+      non-physical, creative labor (i.e. web design, marketing, SEO, upgrades);</li>
+      <li>connects freelancers to their local farmers in an authentic way, which can
+      foster a sense of place, belonging and community;</li>
+      <li>provides farmers with needed services while removing financial barriers to access;</li>
+      <li>creates a wellspring of other paid and service opportunities for the freelancer with
+      others who are also connected to the farm, leading to more business and
+      clients;</li>
+      <li>provides freelancer with locally grown, organic foods that could
+      improve wellbeing;</li>
+      <li>promotes the farm to greater visibility, which will
+      help with long-term sustainability; and </li>
+      <li>creates a tipping point for farms to
+      leverage support to secure future funding/grants</li>
+    </p>
   </section>
-  `;
+  <section id="bio">
+    <div id="bioimg"></div>
+    <div id="savvybanner"></div>
+  </section>
+`;
